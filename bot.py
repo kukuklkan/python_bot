bccc51059efd74be99334e790cf51ca1df83ca7c
kukuklkan <yrkaadamcuk675@gmail.com>
2023-06-17 20:49:20 +0200

    all hand made in one!

--- bot.py
+++ bot.py
@@ -30,22 +30,20 @@ def hello(message):
 #   Обробка кнопок
 @bot.callback_query_handler(func=lambda call: True)
 def calldack(call):
-    class Holder():
-        def set(self, value):
-            self.value = value
-        def get(self, get_value = 0):
-            self.get_value = get_value + self.value
-            return self.get_value
-def ensure(x):
-    if x: yield x        
     msg_id = call.message.message_id
     user_name = call.from_user.first_name
     user_id = call.from_user.id
     chat_id = call.message.chat.id
     profile_link = f'<a href="tg://user?id={user_id}">{user_name}</a>'
+    global cost_of_handmade
     cost_of_handmade = Holder()
+    cost_of_handmade.set(120)
+    global result_minutes
     result_minutes = Holder()
+    result_minutes.set(0)
+    global result_gigabytes
     result_gigabytes = Holder()
+    result_gigabytes.set(0)
     if call.message:
         if call.data == 'tariff':
             markup = telebot.types.InlineKeyboardMarkup(row_width=1)
@@ -73,11 +71,17 @@ def ensure(x):
             start_in_hand_made = telebot.types.InlineKeyboardButton('🚀Почнемо складати ваший перснальний тариф!', callback_data='gigabytes_for_hand_made')
             markup.add(start_in_hand_made)
             bot.send_message(chat_id, f'Виберіть одну з опцій на ваш вибір.', reply_markup=markup)
-            result_gigabytes.set(0)
-            result_minutes.set(0)
-
+            with open('gigabytes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':0, 'gigabytes':0}
+                json.dump(info_dict, gigabytes_json)
+            with open('minutes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':0, 'gigabytes':0}
+                json.dump(info_dict, gigabytes_json)
         # Buttons for Gigabytes
         elif call.data == 'gigabytes_for_hand_made':
+            with open('gigabytes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':0, 'gigabytes':0}
+                json.dump(info_dict, gigabytes_json)
             markup = telebot.types.InlineKeyboardMarkup(row_width=4)
             zero_gigabates = telebot.types.InlineKeyboardButton('0️⃣', callback_data='sub_result_zero')
             four_gigabates = telebot.types.InlineKeyboardButton('4️⃣', callback_data='sub_result_four')
@@ -88,38 +92,45 @@ def ensure(x):
         # Sub level in taking gigabytes
         elif call.data == 'sub_result_zero':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_gigabytes.set(0)
-            cost_of_handmade.set(0)
+            with open('gigabytes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':0, 'gigabytes':0}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='minutes_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='gigabytes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 0 Гігабайтів для користування!', reply_markup=markup)
         elif call.data == 'sub_result_four':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_gigabytes.set(4)
-            cost_of_handmade.get(40)
+            with open('gigabytes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':40, 'gigabytes':4}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='minutes_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='gigabytes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 4 Гігабайтів для користування!', reply_markup=markup)
         elif call.data == 'sub_result_ten':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_gigabytes.set(10)
-            cost_of_handmade.get(45)
+            with open('gigabytes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':45, 'gigabytes':10}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='minutes_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='gigabytes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 10 Гігабайтів для користування!', reply_markup=markup)
         elif call.data == 'sub_result_twenty':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_gigabytes.set(20)
-            cost_of_handmade.get(105)
+            with open('gigabytes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':105, 'gigabytes':20}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='minutes_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='gigabytes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 20+ Гігабайтів для користування!', reply_markup=markup)
         # Button for Minutes
         elif call.data == "minutes_for_hand_made":
+            with open('minutes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':0, 'gigabytes':0}
+                json.dump(info_dict, gigabytes_json)
             markup = telebot.types.InlineKeyboardMarkup(row_width=4)
             zero_minutes = telebot.types.InlineKeyboardButton('0️⃣', callback_data='sub_result_zero_minutes')
             hundred_minutes = telebot.types.InlineKeyboardButton('1️⃣0️⃣0️⃣', callback_data='sub_result_hundred_minutes')
@@ -130,44 +141,54 @@ def ensure(x):
         # Sub level of taking minutes
         elif call.data == 'sub_result_zero_minutes':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_minutes.set(0)
-            cost_of_handmade.get(0)
+            with open('minutes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':0, 'minutes':0}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='taryf_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='minutes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 0 Хвилин для користування!', reply_markup=markup)
         elif call.data == 'sub_result_hundred_minutes':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_minutes.set(100)
-            cost_of_handmade.get(30)
+            with open('minutes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':30, 'minutes':100}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='taryf_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='minutes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 100 Хвилин для користування!', reply_markup=markup)
         elif call.data == 'sub_result_twohundred_minutes':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_minutes.set(200)
-            cost_of_handmade.get(50)
+            with open('minutes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':50, 'minutes':200}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='taryf_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='minutes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 200 Хвилин для користування!', reply_markup=markup)
         elif call.data == 'sub_result_fivehundred_minutes':
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-            result_minutes.set(500)
-            cost_of_handmade.get(140)
+            with open('minutes.json', 'w') as gigabytes_json:
+                info_dict = {'cost':140, 'minutes':500}
+                json.dump(info_dict, gigabytes_json)
             sub_result = telebot.types.InlineKeyboardButton('🚀Продовжимо', callback_data='taryf_for_hand_made')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='minutes_for_hand_made')
             markup.add(sub_result, back)
             bot.send_message(chat_id, f'Було вибрано 500 Хвилин для користування!', reply_markup=markup)
         # Result of choosing
-        elif call.data == 'taryf_for_hand_made':
+        if call.data == 'taryf_for_hand_made':
+            with open('minutes.json', 'r') as minutes_json:
+                minutes_cost = json.load(minutes_json)
+            with open('gigabytes.json', 'r') as gigabytes_json:
+                gigabytes_cost = json.load(gigabytes_json)
+            result_in_gigabytes = gigabytes_cost['gigabytes']
+            result_in_minutes = minutes_cost['minutes']
+            result_in_cost = gigabytes_cost['cost'] + minutes_cost['cost'] + 120
             markup = telebot.types.InlineKeyboardMarkup(row_width=2)
             change_taryf = telebot.types.InlineKeyboardButton('♻️Змінити данні тарифу', callback_data='handmade')
             back = telebot.types.InlineKeyboardButton('🔙Повернутися назад', callback_data='tariff')
             markup.add(change_taryf, back)
-            print(result_gigabytes.get())
-            bot.send_message(chat_id, f'Тариф який ви зібрали:\nГігабайти: {result_gigabytes.get()}\nХвилини: {result_minutes.get()}\nЦіна вашого тарифу: {cost_of_handmade.get()}грн.\nДля зміни тарифу натисніть нижче (Змінити данні тарифу) або для вибору тарифу з наявних натисніть (Повернутися назад).', reply_markup = markup)
+            bot.send_message(chat_id, f'Тариф який ви зібрали:\nГігабайти: {result_in_gigabytes}\nХвилини: {result_in_minutes}\nЦіна вашого тарифу: {result_in_cost}грн.\nДля зміни тарифу натисніть нижче (Змінити данні тарифу) або для вибору тарифу з наявних натисніть (Повернутися назад).', reply_markup = markup)
         elif call.data == 'one':
             filtered_tariffs = [tariff for tariff in tariffs if tariff[list(tariff.keys())[0]]['gigabytes'] >= 1]
         elif call.data == 'two':
